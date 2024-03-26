b514fdff086ef1aeca96c32e19b616a097100bbf
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-03-26 01:14:08 -0400

    add cookies

---
+++ server/routes/survey.js
@@ -0,0 +1,62 @@
+import express from "express";
+
+// database
+import db from "../db/connection.js";
+const router = express.Router();
+
+// This section will help you get a single record by id
+router.post("/", async (request, res) => {
+
+    let collection = await db.collection("survey-results");
+    const prolificID = request.body.prolificID;
+
+    const record = await collection.findOne({ PID: prolificID});
+  
+    if (!record) 
+    {
+        console.log("in record: ",)
+        res.send({ survey: {}, sessionData: prolificID }).status(200);
+    }
+    else {
+        const survey = record.survey;
+        return { survey: survey, sessionData: prolificID };
+    }
+  });
+
+
+
+router.patch("/", async (request, res) => {
+    try {
+        // data from fetch
+        const data = request.body;
+        const collection = await db.collection("survey-results");
+
+        // get survey info and prolificID and store it in JSON
+        const jsonData = JSON.stringify(data.surveyData);
+        const prolificID = data.prolificID;
+        const surveyData = {
+            PID: prolificID,
+            survey: jsonData,
+        };
+
+        const p = await collection.findOne({ PID: prolificID });
+        if (p) {
+            const result = await collection.updateOne(
+            { PID: prolificID },
+            { $set: { survey: jsonData } }
+            );
+            return result;
+        } else {
+            const result = await collection.insertOne(surveyData);
+            res.send(result).status(200);
+        }
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating record");
+    }
+});
+
+
+export default router;
--- src/pages/surveyPage.jsx
+++ src/pages/surveyPage.jsx
@@ -2,6 +2,10 @@
 import { Survey } from 'survey-react-ui';
 import { Model } from 'survey-core';
 import surveyJson from '../survey';
+import {useEffect } from 'react';
+
+// cookie
+import Cookies from 'js-cookie';
 
 // other components
 import TopBar from "../web-components/TopBar";
@@ -11,15 +15,62 @@ import TopBar from "../web-components/TopBar";
 //import { ExamConfirmationButton } from "../survey-components/ExamConfirmationButton";
 //import { ExamNextButton } from "../survey-components/NextButton";
 
+// good resource: https://github.com/mongodb-developer/mern-stack-example/
+
+
+async function fetchData(cookieID) {
+  // cookie data
+  const response = await fetch(`http://localhost:8080/postsurvey`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prolificID: cookieID}),
+  })
+  if (!response.ok) {
+    const message = `An error occurred: ${response.statusText}`;
+    console.error(message);
+    return { survey: {}, sessionData: cookieID };
+  }
+  else {
+    const data = await response.json();
+    console.log(data)
+    const survey = data.survey;
+    return { survey, sessionData: cookieID };
+  }
+
+}
+
 
 export default function SurveyPage() {
 
-    const survey = new Model(surveyJson);
-    
-    // custom widgets
-    //ExamConfirmationButton(survey);
-    //CameraConfirmationButton(survey);
-    //ExamNextButton(survey);
+  const cookieID = Cookies.get("prolificID");
+  const survey = new Model(surveyJson);
+  const data = fetchData(cookieID);
+  survey.data = data.survey;
+
+  // custom widgets
+  //ExamConfirmationButton(survey);
+  //CameraConfirmationButton(survey);
+  //ExamNextButton(survey);
+  
+  useEffect(() => {
+    // Update data on server with better error handling
+    const handleValueChanged = async () => {
+      const cData = Cookies.get("prolificID");
+      const updatedData = survey.data;
+      await fetch("http://localhost:8080/postsurvey", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prolificID: cData, surveyData: updatedData }),
+      });
+    };
+  
+    survey.onValueChanged.add(handleValueChanged);
+    }, [survey]);
+
     
     return (
       <>
