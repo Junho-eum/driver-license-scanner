86ad8199da8334451c4527081e565da456243fd7
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-05-20 15:09:17 -0400

    add script to dump db to csv

---
+++ mongo_to_csv.js
@@ -0,0 +1,21 @@
+// database
+import db from "./server/db/connection.js";
+import csv from 'json2csv';
+import fs from 'fs';
+
+const collection = await db.collection("survey-results");
+
+// replace with appropriate name
+const outputFile = "prolific_survey_data.csv";
+
+const results = await collection.find().toArray((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
+
+const csvData = csv.parse(results);
+fs.writeFileSync(outputFile, csvData);
+
+console.log(`Survey results exported to ${outputFile}`)
