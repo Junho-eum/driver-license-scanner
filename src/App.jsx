a7336b53e85f032382ca9cbd0736a6a9e3901c61
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-05-30 13:38:37 -0400

    fix #24

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ export default function App() {
                 Prolific ID
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-60 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="id"
                 name="prolificID"
                 value={prolificID}
--- src/survey-components/ConfirmCamera.jsx
+++ src/survey-components/ConfirmCamera.jsx
@@ -9,7 +9,7 @@ export function CameraConfirmationButton(Survey) {
     // Check if the widget applies to the current question
     isFit: (question) => question.name === componentName,
     htmlTemplate:
-      "<div style='height: 39px'>" +
+      "<div>" +
       "<button type='button' id='confirm_camera' class='bg-gw-primary-blue rounded px-8 py-4 mb-5 shadow-lg text-2xl font-extrabold text-white'> Confirm Camera Permissions </button>" +
       "&nbsp;&nbsp;" +
       "</div>",
